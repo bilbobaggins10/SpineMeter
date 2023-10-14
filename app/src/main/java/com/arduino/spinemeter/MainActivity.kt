b83731b51b851b6a098b778bf97af3ee71578d35
Professional <vadim.kovyrov@aon.at>
2023-10-14 23:08:25 +0200

    Initial commit

---
+++ app/src/main/java/com/arduino/spinemeter/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.arduino.spinemeter
+
+import android.bluetooth.BluetoothManager
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.getSystemService
+import com.arduino.spinemeter.ui.theme.SpineMeterTheme
+import com.arduino.spinemeter.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        setContent {
+//            SpineMeterTheme {
+////                 A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Greeting("Android bla")
+//                }
+//            }
+//        }
+
+        val bluetoothManager = getSystemService(
+            BluetoothManager::class.java
+        )
+        val bluetoothAdapter = bluetoothManager.adapter
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SpineMeterTheme {
+        Greeting("Android")
+    }
+}
